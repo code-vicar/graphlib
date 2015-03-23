00bf8190dd0d8b612202939e213544c5f15b120f
Scott Vickers <scott.w.vickers@gmail.com>
2015-03-23 09:59:51 -0700

    bfs update: add vertex and edge processing callback, add cycle count

--- test/test.js
+++ test/test.js
@@ -25,7 +25,7 @@
     }
 
     // weighted directed graph
-    var directed = new Graph(10, {
+    var directed = new Graph(vertices, {
         EdgeClass: WeightedEdge,
         directed: true
     });
@@ -40,15 +40,26 @@
     directed.insertEdge(vertices[0], vertices[3]);
     directed.insertEdge(vertices[3], vertices[4]);
     directed.insertEdge(vertices[2], vertices[5]);
+    directed.insertEdge(vertices[5], vertices[2]);
 
     // console.log(util.inspect(directed, {
     //     depth: 4
     // }));
 
-    // var Edge = EdgeFactory.defineEdge();
+    // var bfs = new BFS(directed, 2, {
+    //     processEdge: function (v, edge) {
+    //         console.log(v, util.inspect(edge, {
+    //             depth: 4
+    //         }));
+    //     }
+    // });
+
+    // console.log(util.inspect(bfs, {
+    //     depth: 4
+    // }));
 
     // unweighted undirected graph
-    var undirected = new Graph(10);
+    var undirected = new Graph(vertices);
 
     undirected.insertEdge(vertices[0], vertices[1]);
     undirected.insertEdge(vertices[0], vertices[2]);
@@ -62,7 +73,13 @@
         depth: 4
     }));
 
-    var bfs = new BFS(undirected, 0);
+    var bfs = new BFS(undirected, 4, {
+        processEdge: function (v, edge) {
+            console.log(v, util.inspect(edge, {
+                depth: 4
+            }));
+        }
+    });
 
     console.log(util.inspect(bfs, {
         depth: 4
