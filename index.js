85eb583318eee4c9f09c2bf7ae6f8b96311edb96
Scott Vickers <svickers@weareratio.com>
2016-06-29 20:33:36 -0700

    fix main index exports

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var BFS = require('./dist/traversal/bfs');
 var DFS = require('./dist/traversal/dfs');
 
 module.exports = {
-    Graph: Graph,
-    findPath: findPath,
-    BFS: BFS,
-    DFS: DFS
+    Graph: Graph.default,
+    findPath: findPath.default,
+    BFS: BFS.default,
+    DFS: DFS.default
 }
